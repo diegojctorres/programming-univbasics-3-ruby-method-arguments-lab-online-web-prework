79abb5c9f8e73418c43b450da9164712f0647ab3
diegojctorres <diegojctorres@gmail.com>
2019-07-10 03:44:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name)
-  "Hi, my name is #{name}."
+  puts "Hi, my name is #{name}"
 end
 
 def introduction_with_language(name, language)
@@ -9,3 +9,4 @@ end
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
